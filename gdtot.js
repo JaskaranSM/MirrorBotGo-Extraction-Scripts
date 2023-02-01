3bc0ace19b10399b597f850ea02d60b3771b3608
jaskaranSM <jaskaransembhi80@gmail.com>
2023-02-01 08:13:52 -0500

    update gdtot

--- gdtot.js
+++ gdtot.js
@@ -1,10 +1,22 @@
 //https:\/\/.+gdtot.+\/file\/.+
 function extract(url) {
-	return request({
-		url: url.replace("file","ddl"),
-		method: "GET",
-		cookies: {
-			crypt: gdtot_crypt
-		}
-	}).bodyText.match(/.+myDl\('(.+)'\)/)[1]
+    return request({
+        url: request({
+            url: request({
+                url: url,
+                method: "GET",
+                cookies: {
+                    crypt: gdtot_crypt
+                }
+            }).bodyText.match(/.+myDl\('(.+)'\)/)[1],
+            method: 'GET',
+            cookies: {
+                crypt: gdtot_crypt
+            }
+        }).bodyText.match(/URL=(.+)\"/)[1],
+        method: 'GET',
+        cookies: {
+            crypt: gdtot_crypt
+        }
+    }).bodyText.match(/(https:\/\/drive\.google\.com.+)"\s/)[1]
 }
