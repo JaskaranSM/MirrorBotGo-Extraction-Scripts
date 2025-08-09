49c25dc71c6bdfc516d3f4375d3267f3f8a99c19
JaskaranSM <jaskaransembhi80@gmail.com>
2025-08-09 12:16:50 -0400

    update hubcloud

--- hubcloud.js
+++ hubcloud.js
@@ -1,10 +1,34 @@
-//https:\/\/hubcloud\.(.+)\/drive\/.+
+//https:\/\/(?:hubcloud\.(.+)\/drive\/.+|vifix\.site\/hubcloud\/([a-z0-9]+))
 function extract(url) {
-    return findNodes(request({
-        url: findNodes(request({
-            url: url,
+    return findNodes(
+        request({
+            url: findNodes(
+                request({
+                    url: /^https:\/\/vifix\.site\/hubcloud\/([a-z0-9]+)$/i.test(url)
+                        ? `https://hubcloud.one/drive/${url.split("/").pop()}`
+                        : url,
+                    method: "GET"
+                }).bodyText,
+                "a"
+            ).find(node => node.attributes.id == "download").attributes.href,
             method: "GET"
-        }).bodyText, "a").find(node => node.attributes.id == "download").attributes.href,
-        method: "GET"
-    }).bodyText, "a").find(node => node.attributes.href.includes("r2.dev")).attributes.href
+        }).bodyText,
+        "a"
+    ).map(n => {
+        let href = n.attributes.href;
+        if (!href) return null;
+
+        if (href.includes("r2.dev")) {
+            return href;
+        }
+        if (href.includes("pixeldrain")) {
+            if (href.includes("/u/")) {
+                return "https://pixeldrain.net/api/file/" + href.split("/u/").pop();
+            }
+            if (href.includes("/api/file/")) {
+                return href;
+            }
+        }
+        return null;
+    }).find(Boolean);
 }
